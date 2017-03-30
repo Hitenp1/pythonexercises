5bfb814e75ad8ad39dbcdf5250b4822ff3a93912
Hitenp1 <hpduke2@gmail.com>
2017-03-30 15:07:25 -0400

    twoconcepts

--- main.py
+++ main.py
@@ -22,5 +22,18 @@ print (list(map(lambda x: x**2, nums)))
 print (sum(nums))
 # string of nums sum(int(i) for i in nums)
 
+def divisors (num):
+  arr = []
+  for item in range(1,num+1):
+    if num % item == 0:
+      arr.append(item)
+  print (arr)
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+#find values that are in both lists
+
+print([x for x in b if x in a])
 
 
