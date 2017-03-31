355cb6694ec199028b73ca639e1201810e227326
Hitenp1 <hpduke2@gmail.com>
2017-03-31 18:44:12 -0400

    rps game

--- main.py
+++ main.py
@@ -36,4 +36,39 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 print([x for x in b if x in a])
 
+#rockPaperscissors
+
+import random
+
+def rps() :
+
+  game_dict = {'rock': 1, 'scissors': 2, 'paper': 3}
+  comp = random.randint(0, 3)
+  print(comp)
+  choice = input('Player1 Enter RPS:').lower()
+  player1 = game_dict[choice]
+  dif = comp - player1
+
+  def playagain():
+    resp = input('Play Again?').lower()
+    if(resp.lower()[0] == 'y'):
+      rps()
+    else:
+      print('Thanks for playing')
+
+  if dif in [-1, 2]:
+    print('You chose {} and the computer chose {}'.format(choice, list(game_dict.keys())[list(game_dict.values()).index(comp)] ))
+    print('Computer Wins')
+    playagain()
+  elif dif in [-2, 1]:
+     print('You chose {} and the computer chose {}'.format(choice, list(game_dict.keys())[list(game_dict.values()).index(comp)] ))
+     print('You win')
+     playagain()
+  else:
+     print('Tie')
+     print('You both chose {}'.format(choice))
+     playagain()
+
+rps()
+
 
